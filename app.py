03267614fe636eebde6f497fb2791fd1d90ba693
Lucas <lucas.valdetario@gmail.com>
2024-11-30 20:22:43 -0300

    Updating

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import flask
+
+app = flask(__name__)
+
+''' 1 - Deve ser possível registrar uma refeição feita, com as seguintes informações:
+    - Nome
+    - Descrição
+    - Data e Hora
+    - Está dentro ou não da dieta'''
+    
+'''2 - Deve ser possível editar uma refeição, podendo alterar todos os dados acima'''
+
+'''3 - Deve ser possível apagar uma refeição'''
+
+'''4 - Deve ser possível listar todas as refeições de um usuário'''
+
+'''5 - Deve ser possível visualizar uma única refeição'''
+
+'''6 - As informações devem ser salvas em um banco de dados'''
+
+if __name__ == "__name__":
+    app.run(debug=True)
